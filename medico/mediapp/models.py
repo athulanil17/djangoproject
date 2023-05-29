de98b6080953293ec74d85333bc4e41772d44a68
athul.geetha456@gmail.com <athul.geetha456@gmail.com>
2023-05-29 12:40:25 +0530

    Project added

---
+++ medico/mediapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Medicine(models.Model):
+    name=models.CharField(max_length=50)
+    price=models.PositiveIntegerField()
+    quantity=models.PositiveIntegerField()
+    expiry=models.DateField()
+    description=models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,default=1)
